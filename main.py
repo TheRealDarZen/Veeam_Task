0f046e7ed5e258e4c693fccb6004f2a8d159ae2f
DarZen <nivnik004@gmail.com>
2025-07-14 22:20:13 +0200

    Added a loop with code logic. sync() function will be handling file synchronization and logging (not implemented yet)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import hashlib
 import sys
-
-
+from pathlib import Path
+from time import sleep
 
 
 def get_file_hash(path):
@@ -9,31 +9,41 @@ def get_file_hash(path):
         hash = hashlib.sha256(binFile.read())
         return hash.hexdigest()
 
-def create_source_tree(current_path):
 
+def create_tree(path):
     tree = {}
 
-    for entry in current_path.iterdir():
+    for entry in path.iterdir():
         if entry.is_file():
             hash = get_file_hash(entry)
             tree[entry.name] = hash
         elif entry.is_dir():
-            tree[entry.name] = create_source_tree(current_path / entry.name)
+            tree[entry.name] = create_tree(path / entry.name)
         else:
             raise FileNotFoundError("An entry isn't marked as file nor directory.")
 
     return tree
 
 
+def sync(source_path, replica_path, source_tree, replica_tree, log_path):
+    pass
+
+
 
 def main():
-    PATH_TO_SOURCE = sys.argv[0]
-    PATH_TO_REPLICA = sys.argv[1]
+    PATH_TO_SOURCE = Path(sys.argv[0])
+    PATH_TO_REPLICA = Path(sys.argv[1])
     INTERVAL = int(sys.argv[2])
     AMOUNT_OF_SYNCS = int(sys.argv[3])
-    PATH_TO_LOG_FILE = sys.argv[4]
+    PATH_TO_LOG_FILE = Path(sys.argv[4])
 
+    replica_tree = {}
 
+    for _ in range(AMOUNT_OF_SYNCS):
+        sleep(INTERVAL)
+        source_tree = create_tree(PATH_TO_SOURCE / 'source')
+        sync(PATH_TO_SOURCE, PATH_TO_REPLICA, source_tree, replica_tree, PATH_TO_LOG_FILE)
+        replica_tree = source_tree.copy()
 
 
 
